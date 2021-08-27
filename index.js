71e4364b9d1d40777b4ffe2d890344bd5d6fc425
burnworks <6436833+burnworks@users.noreply.github.com>
2021-08-27 21:26:26 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+module.exports = function ({ addUtilities }) {
+    const animationDelay = {
+        '.animation-delay-75': {
+            animationDelay: '75ms',
+        },
+        '.animation-delay-100': {
+            animationDelay: '100ms',
+        },
+        '.animation-delay-150': {
+            animationDelay: '150ms',
+        },
+        '.animation-delay-200': {
+            animationDelay: '200ms',
+        },
+        '.animation-delay-300': {
+            animationDelay: '300ms',
+        },
+        '.animation-delay-400': {
+            animationDelay: '400ms',
+        },
+        '.animation-delay-500': {
+            animationDelay: '500ms',
+        },
+        '.animation-delay-600': {
+            animationDelay: '600ms',
+        },
+        '.animation-delay-700': {
+            animationDelay: '700ms',
+        },
+        '.animation-delay-800': {
+            animationDelay: '800ms',
+        },
+        '.animation-delay-900': {
+            animationDelay: '900ms',
+        },
+        '.animation-delay-1000': {
+            animationDelay: '1000ms',
+        },
+    }
+    addUtilities(animationDelay)
+}
